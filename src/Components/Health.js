af470987ec960c42a32d043963d9c67a7ea12156
Anshul Aggarwal <aggarwalanshul1997@gmail.com>
2023-03-26 19:45:54 +0530

    final changes

--- src/Components/Health.js
+++ src/Components/Health.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import Newsitem from "./Newsitem";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Sports(props) {
+export default function Health(props) {
   const [sports, setsports] = useState([]);
   const updateNews = async () => {
     let url =
-      `https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=f97d4d6864824d38a5b85be62b98e1dd&pageSize=${props.pagesize}`;
+      `https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=f97d4d6864824d38a5b85be62b98e1dd&pageSize=20`;
     let data = await fetch(url);
     let parsedData = await data.json();
     setsports(parsedData.articles);
@@ -19,11 +19,7 @@ export default function Sports(props) {
   return (
     <>
       <div className="container" style={{backgroundColor:`${props.bgcolor}`}}>
-        <div className="tagline"><h2 style={{color:`${props.textcolor}`}}>{props.heading} &gt;  </h2> 
-        <a href="" className="btn btn-primary">
-            Read More
-          </a></div>
-        
+        <h2 style={{color:`${props.textcolor}`}}>{props.heading} &gt;  </h2> 
         <div className="row" >
           {sports.map((element) => {
             return (
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,33 @@
 import React from 'react'
+// import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">NewsViews</a>
+          <a className="navbar-brand" href="#">NewsViews.com</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              
+            
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link" href="#">Economy</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <a className="nav-link" href="#">Sports</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="">Health</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Technology</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">International</a>
               </li>
             </ul>
             <form className="d-flex" role="search">
